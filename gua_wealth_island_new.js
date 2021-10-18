146118bb40eafcbb463bb3053508b43d46783a08
yyetss <757542306@qq.com>
2021-10-18 16:16:41 +0800

    Update gua_wealth_island_new.js

--- gua_wealth_island_new.js
+++ gua_wealth_island_new.js
@@ -31,7 +31,7 @@ $.innerInviteList = [];
 const HelpAuthorFlag = false;//是否助力作者SH  true 助力，false 不助力
 
 // 热气球接客 每次运行接客次数
-let serviceNum = 10;// 每次运行接客次数
+let serviceNum = 20;// 每次运行接客次数
 if ($.isNode() && process.env.gua_wealth_island_serviceNum) {
   serviceNum = Number(process.env.gua_wealth_island_serviceNum);
 }
